4f762861e267761aeb95bd4db6257ff337723774
concon0711 <125149625+concon0711@users.noreply.github.com>
2023-04-23 19:08:58 -0400

    Update user.controller.test.js

--- backend/controllers/user.controller.test.js
+++ backend/controllers/user.controller.test.js
@@ -1,54 +1,53 @@
-import supertest from 'supertest'
-import usercontroller from './user.controller.js'
+const db = require('../config/db.config');
+const { SignUp, Login, AdminLogin, GetUserInfo } = require('./user.controller');
 
-describe("POST /user"), () => {
+jest.mock('../config/db.config');
 
-    describe("given customer user and password", () => {
-        //save customer user and password
-        //respond with status 200 to know success
-        test("should respond with 200 code", async() => {
-            const response = await request(usercontroller).post("/user").send({
-                username: "Username",
-                password: "Password"
-            })
-            expect(response.statusCode).tobe(200)
-        })
-        //response when successful 
-        test("should specify json in content type header", async () => {
-             const response = await request(usercontroller).post("/user").send({
-                username: "Username",
-                password: "Password"    
-            })
-            expect(response.header['content-type']).toEqual(expect.stringContaining("json")) 
-        })
-        test("respone has userId", async () =>{
-            const response = await request(usercontroller).post("/users").send({
-                username: "Username",
-                password: "Password"
-            })
-            expect(response.body.userId).toBeDefined()
-        })
-       
-    })
+describe('SignUp', () => {
+  it('should call the database with the correct query', () => {
+    const req = { body: { Username: 'testuser', Password: 'testpassword' } };
+    const res = { send: jest.fn() };
+    const queryMock = jest.fn();
 
-    describe(" when user and password is missing", () => {
-        test("response is 400", async () => {
-             //respond code 400 to know error
-        //fail to add user or password 
+    db.query.mockImplementation(queryMock);
 
-            const bodyData = [
-                {username: "Username"},
-                {password: "Password"},
-                {}
-            ]
-            for (const body of bodyData){
-            const response = await request(usercontroller).post("/users").send(body)
-            expect(response.statusBody).toBe(400)
-           }   
-        })
-        //respond code 400 to know error
-        //fail to add user or password 
+    SignUp(req, res);
+
+    expect(queryMock).toHaveBeenCalledWith(
+      "CALL CustomerSignUp('testuser', 'testpassword');",
+      expect.any(Function),
+    );
+  });
+
+  it('should return a response with data if the database query succeeds', () => {
+    const req = { body: { Username: 'testuser', Password: 'testpassword' } };
+    const res = { send: jest.fn() };
+    const queryMock = jest.fn((_, callback) => {
+      const data = { message: 'success' };
+      callback(null, data);
+    });
+
+    db.query.mockImplementation(queryMock);
+
+    SignUp(req, res);
+
+    expect(res.send).toHaveBeenCalledWith({ message: 'success' });
+  });
+
+  it('should return an error response if the database query fails', () => {
+    const req = { body: { Username: 'testuser', Password: 'testpassword' } };
+    const res = { status: jest.fn().mockReturnThis(), end: jest.fn() };
+    const queryMock = jest.fn((_, callback) => {
+      callback(new Error('database error'), null);
+    });
+
+    db.query.mockImplementation(queryMock);
+
+    SignUp(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.end).toHaveBeenCalled();
+  });
+});
 
 
-    })
-}
