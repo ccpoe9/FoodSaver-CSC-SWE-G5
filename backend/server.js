5884cd58f9270486be9adb4b22e6b4012041c919
ccpoe9 <chikapaulezeh@gmail.com>
2023-04-23 02:04:06 +0000

    set up secure ssl https

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,31 @@ const bodyparser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000
 let router = require('./routes/router');
-
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const cors = require('cors');
 const corsOptions ={
     origin:'http://localhost:4200', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
+
 app.use(cors(corsOptions));
 
 app.use(bodyparser.json());
 app.use('/', router);
 
-app.listen(port, () => {
-  console.log(`FoodSaver API listening on port ${port}`);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/foodsaverapi.one/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/foodsaverapi.one/fullchain.pem'),
+}, app);
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
 });
